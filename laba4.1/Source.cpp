2713fa8f11215ed663cfccf42f4f5ac5beee4357
StanislavSorochak <75738611+StanislavSorochak@users.noreply.github.com>
2020-12-12 01:11:27 +0200

    first master commit

---
+++ laba4.1/Source.cpp
@@ -0,0 +1,50 @@
+// Lab_04_1.cpp
+// < Сорочак Станіслав >
+// Лабораторна робота № 4.1
+// Цикли.
+// Варіант 24
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int i, N;
+	double S;
+	
+	cout << "N = "; cin >> N;
+
+	S = 0;
+	i = 1;
+	while (i <= N)
+	{
+		S += (sqrt(1 + (cos(sin(1. * i))) * (cos(sin(1. * i))))) / (1 + (sin(cos(1. * i))) * (sin(cos(1. * i))));
+		i++;
+	}
+	cout << S << endl;
+	S = 0;
+	i = 1;
+	do {
+		S += (sqrt(1 + (cos(sin(1. * i))) * (cos(sin(1. * i))))) / (1 + (sin(cos(1. * i))) * (sin(cos(1. * i))));
+		i++;
+	} while (i <= N);
+	cout << S << endl;
+
+	S = 0;
+	for (i = 1; i <= N; i++)
+	{
+		S += (sqrt(1 + (cos(sin(1. * i))) * (cos(sin(1. * i))))) / (1 + (sin(cos(1. * i))) * (sin(cos(1. * i))));
+	}
+	cout << S << endl;
+
+	S = 0;
+	for (i = N; i >= 1; i--)
+	{
+		S += (sqrt(1 + (cos(sin(1. * i))) * (cos(sin(1. * i))))) / (1 + (sin(cos(1. * i))) * (sin(cos(1. * i))));
+	}
+	cout << S << endl;
+
+	return 0;
+}
